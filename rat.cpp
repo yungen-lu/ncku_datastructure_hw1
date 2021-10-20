4144437a9e68e4743f35ef2e9b25dff56d64010c
yungen-lu <70182238+yungen-lu@users.noreply.github.com>
2021-10-20 21:15:37 +0800

    fileio as class

--- rat.cpp
+++ rat.cpp
@@ -71,7 +71,49 @@ private:
     std::cout << counter++ << ':' << tmp.x << ',' << tmp.y << std::endl;
   }
 };
+class FileIO {
+public:
+  bool maze[17][17] = {{false}};
+  void getFileName() {
+    std::cout << "enter filename : ";
+    std::cin >> fileName;
+  }
+  void openAndConvert() {
+    fileBuffer.open(fileName);
+    // bool maze[17][17] = {{false}};
+    if (fileBuffer.is_open()) {
+      convertStringToInt();
+      printMaze(maze);
+    } else {
+      std::cerr << "can't open file name" << fileName << std::endl;
+    }
+    fileBuffer.close();
+  }
+  void convertStringToInt() {
+    std::string buffer;
+    int x = 0;
+    while (std::getline(fileBuffer, buffer)) {
+      for (int i = 0; i < buffer.size(); i++) {
+        if (buffer[i] == '1') {
+          maze[x][i] = true;
+        }
+      }
+      x++;
+    }
+  }
+  void printMaze(bool (&maze)[17][17]) {
+    for (int i = 0; i < 17; i++) {
+      for (int j = 0; j < 17; j++) {
+        std::cout << maze[i][j];
+      }
+      std::cout << std::endl;
+    }
+  }
 
+private:
+  std::string fileName;
+  std::ifstream fileBuffer;
+};
 void convertCharToInt(std::ifstream &fileBuffer, bool (&maze)[17][17]);
 void printMaze(bool (&maze)[17][17]);
 // bool sol(int (&maze)[17][17], int startPosX, int startPosY, int exitPosX,
@@ -80,41 +122,49 @@ void printMaze(bool (&maze)[17][17]);
 void print(item tmp);
 int main() {
   // std::string fileName = "maze.txt";
-  std::string fileName;
-  std::cout << "enter filename : ";
-  std::cin >> fileName;
-  std::ifstream fileBuffer;
-  fileBuffer.open(fileName);
-  bool maze[17][17] = {{false}};
-  if (fileBuffer.is_open()) {
-    convertCharToInt(fileBuffer, maze);
-    printMaze(maze);
-    while (true) {
-      int startPosX, startPosY, exitPosX, exitPosY;
-      std::cout << "enter start position : ";
-      std::cin >> startPosX >> startPosY;
-      if (startPosX == -1 && startPosY == -1) {
-        std::cout << "end the code." << std::endl;
-        return 0;
-      }
-      if (maze[startPosX][startPosY]) {
-        std::cerr << "unvalid position" << std::endl;
-        return 1;
-      }
-      std::cout << "enter exit position : ";
-      std::cin >> exitPosX >> exitPosY;
-      if (maze[exitPosX][exitPosY]) {
-        std::cerr << "unvalid position" << std::endl;
-        return 1;
-      }
-      Solution sol(maze);
-      sol.sol(startPosX, startPosY, exitPosX, exitPosY);
-      // sol(maze, startPosX, startPosY, exitPosX, exitPosY);
+  // std::string fileName;
+  // std::cout << "enter filename : ";
+  // std::cin >> fileName;
+  // std::ifstream fileBuffer;
+  // fileBuffer.open(fileName);
+  // bool maze[17][17] = {{false}};
+  FileIO test;
+  test.getFileName();
+  test.openAndConvert();
+  // if (fileBuffer.is_open()) {
+  //   convertCharToInt(fileBuffer, maze);
+  //   printMaze(maze);
+  while (true) {
+    int startPosX, startPosY, exitPosX, exitPosY;
+    std::cout << "enter start position : ";
+    std::cin >> startPosX >> startPosY;
+    if (startPosX == -1 && startPosY == -1) {
+      std::cout << "end the code." << std::endl;
+      return 0;
     }
-  } else {
-    std::cerr << "can't open file name" << fileName << std::endl;
-    return 1;
+    if (test.maze[startPosX][startPosY]) {
+      std::cerr << "unvalid position" << std::endl;
+      return 1;
+    }
+    std::cout << "enter exit position : ";
+    std::cin >> exitPosX >> exitPosY;
+    if (startPosX == -1 && startPosY == -1) {
+      std::cout << "end the code." << std::endl;
+      return 0;
+    }
+    if (test.maze[exitPosX][exitPosY]) {
+      std::cerr << "unvalid position" << std::endl;
+      return 1;
+    }
+    Solution sol(test.maze);
+    sol.sol(startPosX, startPosY, exitPosX, exitPosY);
+    // sol(maze, startPosX, startPosY, exitPosX, exitPosY);
   }
+  // }
+  // else {
+  //   std::cerr << "can't open file name" << fileName << std::endl;
+  //   return 1;
+  // }
   return 0;
 }
 void convertCharToInt(std::ifstream &fileBuffer, bool (&maze)[17][17]) {
